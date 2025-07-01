5e5173f28ecfbb4224d915846dfb0b266c0ff721
Vizsy István <steve.visy@gmail.com>
2025-07-01 21:27:59 +0200

    Frontend: installing and setting further development tools

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,42 @@
 import js from "@eslint/js";
 import globals from "globals";
-import markdown from "@eslint/markdown";
+import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
 import css from "@eslint/css";
-//import prettier from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   // Node_modules mappa kizárása
   {
-    ignores: ["node_modules", "src/script.js", "olds"],
+    ignores: ["node_modules"],
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    //plugins: { js, prettier },
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
     rules: {
       "prefer-const": "warn",
       "no-unused-vars": "warn",
-      "eqeqeq": ["warn", "always"]
+      eqeqeq: ["warn", "always"],
       //"prettier/prettier": "warn"
     },
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  {
-    files: ["**/*.md"],
-    plugins: { markdown },
-    language: "markdown/gfm",
-    extends: ["markdown/recommended"],
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
   },
   {
     files: ["**/*.css"],
