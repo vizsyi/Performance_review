203e73ca2a8a307752dd546142b5c5d89d69a48e
Vizsy István <steve.visy@gmail.com>
2025-07-04 16:00:45 +0200

    Frontend: refactoring the backend for naming convention

--- Backend_Lambda/evaluationLambda.mjs
+++ Backend_Lambda/evaluationLambda.mjs
@@ -197,10 +197,10 @@ export const handler = async event => {
       } else if (httpMethod === "POST") {
         const parsedBody = JSON.parse(event.body);
         const employee_id = parsedBody.employee_id || "";
-        const criteria = parsedBody.criteria || [];
+        const evaluation = parsedBody.evaluation || [];
         const is_ready = parsedBody.isready || false;
 
-        if (employee_id === "" || criteria.length === 0) {
+        if (employee_id === "" || evaluation.length === 0) {
           return badRequest(400, "Missing parameters");
         }
 
@@ -210,7 +210,7 @@ export const handler = async event => {
         );
         if (empIndex === -1) return badRequest(404, "Not found");
 
-        const newEvaluation = { employee_id, criteria, is_ready };
+        const newEvaluation = { employee_id, evaluation, is_ready };
         const evaluationsObj = await getEvaluationsObj();
         const index = evaluationsObj.evaluations.findIndex(
           eva => eva.employee_id === employee_id
